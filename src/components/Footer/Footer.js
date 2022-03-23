28ac79251d429b8a591d3d41c52f06f758002115
KEEGAN <59810029+lionelkeegan@users.noreply.github.com>
2022-03-23 22:21:25 +0700

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -13,7 +13,8 @@ function Footer() {
     <div className={styles.container}>
       <footer className={styles.footer}>
         <h2 className={styles.footer__title}>Movie App</h2>
-        <p className={styles.footer__author}>Created by ACHMAD IRVAN FAUZI</p>
+        <p className={styles.footer__author}>Created by ACHMAD IRVAN FAUZI <br></br>
+        <a className={styles.footer__link} href="https://github.com/lionelkeegan">@lionelkeegan</a></p>
       </footer>
     </div>
   );
